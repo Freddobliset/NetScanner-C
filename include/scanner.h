4682e95e17f4146399c6656539ec9164be1a78dc
Freddobliset <mariosale2004@gmail.com>
2026-05-13 19:33:18 +0200

    feat: modified main.c,scanner.c and scanner.h to capture banners

--- include/scanner.h
+++ include/scanner.h
@@ -4,7 +4,8 @@
 #define PORT_OPEN 1
 #define PORT_CLOSED 0
 #define PORT_ERROR -1
+#define BANNER_SIZE 1024
 
-int check_port(const char *ip, int port, int timeout);
+int check_port(const char *ip, int port, char *banner, int banner_size);
 
 #endif // SCANNER_H
--- src/main.c
+++ src/main.c
@@ -10,22 +10,24 @@ int main(int argc, char *argv[]) {
     }
 
     char *target_ip = argv[1];
+    int start = atoi(argv[2]);
+    int end = atoi(argv[3]);
+    char banner[BANNER_SIZE];
+    
+    printf("Scanning target : %s ...\n", target_ip);
+    printf("%-7s | %-8s | %s\n", "PORT", "STATUS", "SERVICE/BANNER");
+    printf("--------------------------------------------------\n");
 
-    int common_ports[] = {21, 22, 23, 25, 53, 80, 110, 143, 443, 3306};
-    int num_ports = sizeof(common_ports) / sizeof(common_ports[0]);
 
-    printf("Scanning %s for common ports...\n", target_ip);
-    for (int i = 0; i < num_ports; i++) {
-        int port = common_ports[i];
-        int result = check_port(target_ip, port, 1000); 
-        if (result == PORT_OPEN) {
-            printf("Port %d is OPEN\n", port);
-        } else if (result == PORT_CLOSED) {
-            printf("Port %d is CLOSED\n", port);
-        } else {
-            printf("Error checking port %d\n", port);
+    for (int p = start; p <= end; p++) {
+        int status = check_port(target_ip, p, banner, BANNER_SIZE);
+        
+        if (status == PORT_OPEN) {
+            printf("%-7d | OPEN     | %s\n", p, banner);
         }
     }
-    printf("Scan complete.\n");
+
+    printf("--------------------------------------------------\n");
+    printf("Scan finished.\n");
     return 0;
 }
