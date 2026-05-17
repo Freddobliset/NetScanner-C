7fc3395d3892517c521de14866c583b971c0f8b5
Freddobliset <mariosale2004@gmail.com>
2026-05-17 17:44:32 +0200

    feat: modified scanner.h file to implement a thread pool

--- include/scanner.h
+++ include/scanner.h
@@ -6,17 +6,24 @@
 #define PORT_ERROR -1
 #define BANNER_SIZE 1024
 #include <stdbool.h>
+#include <pthread.h>
 int check_port(const char *ip, int port, char *banner, int banner_size);
-
-struct  MultiThreadingArgs {
-    char ip[16];
-    int port;
-    char banner[BANNER_SIZE];
-    int banner_size;
-    bool port_status;
-    bool is_udp;
-};
 void* scan_worker(void *args);
-
 int check_udp_port(const char *ip, int port);
+
+typedef struct {
+    char target_ip[16];
+    int start_port;
+    int end_port;
+    int current_port;
+    bool is_udp;
+    pthread_mutex_t *mutex;
+} PortQueue;
+
+typedef struct {
+    PortQueue *queue;
+    char banner[BANNER_SIZE];
+} WorkerArgs;
+
+void* ts_worker_pool(void *args);
 #endif // SCANNER_H
